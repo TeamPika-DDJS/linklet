6b4932b27d51b4ac1da85eb1a4e6997194dbfb46
dison  ruan <disonruan@disons-MacBook-Pro.local>
2023-01-17 18:53:44 -0500

    set up basic routes with no functions

--- server/server.js
+++ server/server.js
@@ -14,6 +14,42 @@ if (MODE === 'production') {
   app.use('/', express.static(path.resolve(__dirname, '../dist')));
 }
 
+/*********************
+    User Routes
+*********************/
+
+// route to create user - sign up
+app.post('/api/users', () => {});
+
+// route to log user in
+
+app.post('api/users/login', () => {});
+
+// route to log user out
+app.post('api/users/logout', () => {});
+
+/*********************
+    List Routes
+*********************/
+
+// route to create new list
+app.post('/api/users/:id/lists', () => {});
+
+// route to read all lists
+app.get('/api/users/:id/lists', () => {});
+
+// route to update list
+app.patch('/api/users/:id/lists', () => {});
+
+// route to delete list
+app.delete('/api/users/:id/lists', () => {});
+
+/*********************
+    Link Routes
+*********************/
+
+// route to create new link - find or create by
+
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}...`);
   console.log(`Currently in ${MODE.toLowerCase()} mode`);
