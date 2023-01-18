0ab26e4f7e01469b5785d832f9af61a98d743136
Stephen <stephen.anthony.rivas@gmail.com>
2023-01-18 08:48:18 -0500

    rendered dynamic routes and lists from urls

--- client/App.js
+++ client/App.js
@@ -1,5 +1,5 @@
 import { stringLiteral } from "@babel/types";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {  
     HashRouter as Router,
     Routes,
@@ -7,8 +7,18 @@ import {
 } from "react-router-dom";
 import Home from "./containers/Home";
 import URLComponent from "./components/URLComponent";
+import ListContainer from "./containers/ListContainer";
 
 const App = () => {
+    // create array of Routes from list of users in user table
+    const [users, setUsers] = useState({
+        1: 'jo',
+        2: 'stephen',
+        3: 'daria',
+        4: 'dyson'
+    });
+
+    const routes = Object.values(users).map((user, i) => <Route key={i} exact path={user} element={<ListContainer userName={user}/>} />);
     return (
         <>
         <Router>
@@ -18,11 +28,7 @@ const App = () => {
                 path="/"
                 element={<Home />}
                 />
-                <Route 
-                exact
-                path="/banana"
-                element={<URLComponent />}
-                />
+                {routes}
             </Routes>
         </Router>
         </>
