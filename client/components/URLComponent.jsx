8a200f91d7d7e2b88723064f1bce3e14e3c3ac7e
Daria Mordvinov <daria.mordvinov@gmail.com>
2023-01-18 16:25:14 -0500

    style

--- client/components/URLComponent.jsx
+++ client/components/URLComponent.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const URLComponent = ({ url }) => {
-    return (
-        <div>
-            <a href={url}>{url}</a>
-        </div>
-    )
-}
+  return (
+    <div className="URL-component">
+      <a href={url}>{url}</a>
+    </div>
+  );
+};
 
 export default URLComponent;
